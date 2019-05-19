1e9796ce1679d259fcb70bb83ad27d02c807b2f9
Marilas Software <mrilassoftware@gmail.com>
2019-05-18 20:34:27 -0500

    Inicial

---
+++ Patinaje/Patinaje.Common/Models/Patinador.cs
@@ -0,0 +1,21 @@
+﻿namespace Patinaje.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Patinador
+    {
+        [Key]
+        public int idPatinador { get; set; }
+
+        public String Identificacion { get; set; }
+
+        public String Nombre { get; set; }
+
+        public String Apellido1 { get; set; }
+
+        public String Apellido2 { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
