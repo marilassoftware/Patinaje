ce27c5db0743ca98c88a098c869e78496d72a0a1
Marilas Software <mrilassoftware@gmail.com>
2019-05-19 14:37:12 -0500

    Se agregaron el login y el admin.

--- Skater.Web/ClientApp/src/app/admin/admin.component.spec.ts
+++ Skater.Web/ClientApp/src/app/admin/admin.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SkaterComponent } from './skater.component';
+import { AdminComponent } from './admin.component';
 
-describe('SkaterComponent', () => {
-  let component: SkaterComponent;
-  let fixture: ComponentFixture<SkaterComponent>;
+describe('AdminComponent', () => {
+  let component: AdminComponent;
+  let fixture: ComponentFixture<AdminComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SkaterComponent ]
+      declarations: [ AdminComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SkaterComponent);
+    fixture = TestBed.createComponent(AdminComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- Skater.Web/ClientApp/src/app/app.module.ts
+++ Skater.Web/ClientApp/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { SkaterComponent } from './skater/skater.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
@@ -18,8 +19,8 @@ import { SkaterComponent } from './skater/skater.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    SkaterComponent
-    
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -29,8 +30,6 @@ import { SkaterComponent } from './skater/skater.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'skater', component: SkaterComponent }
-
     ])
   ],
   providers: [],
